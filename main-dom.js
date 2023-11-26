b07ad4f1612a28079e4613e7b5f419264efdd4d0
daynawhite <dayna26.2@gmail.com>
2023-11-25 18:40:20 -0600

    got everything working in the terminal and started setting up DOM code

--- main-dom.js
+++ main-dom.js
@@ -1,13 +1,3 @@
-const readline = require('readline');
-// use the readline module to print out to the command line
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-
-// let numberArray
-
 // convert the number to an array of its digits
 const numArray = (num) => {
     let numberArray = Array.from(num.toString()).map(Number);
@@ -24,13 +14,6 @@ const rampNum = (numArr) => {
     }}
 }
 }
-// Keep track of how many times rampNum finishes without a false return
-// const rampCount = () => {
-//     if (rampNum(numberArray) != false) {
-//         rampNumberCount++
-//     }
-    
-// }
 // Overall function that will test each number from 1 to the target number:
 const findAllRampNos = (myNum) => {
     let rampNumberCount = 0
@@ -44,12 +27,3 @@ const findAllRampNos = (myNum) => {
     console.log(`${rampNumberCount} total ramp numbers are less than ${myNum}.`)
     return rampNumberCount
 }
-// findAllRampNos(target)
-
-function getPrompt() {
-    rl.question('Enter a number: ', (target) => {
-        findAllRampNos(target);
-        getPrompt();
-    });
-  }
-getPrompt()
